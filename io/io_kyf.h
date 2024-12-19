e361f808503f498ed364f6808772cd61f0fc2d9e
Tomoaki-Ishiyama <ishiyama@chiba-u.jp>
2024-12-20 00:53:26 +0900

    cleaning

--- io/io_kyf.h
+++
@@ -1,9 +0,0 @@
-#ifndef _IO_KYF_H
-#define _IO_KYF_H
-
-#include <stdint.h>
-#include "../particle.h"
-
-void load_particles_kyf(char *filename, struct particle **p, int64_t *num_p);
-
-#endif /* _IO_KYF_H */
