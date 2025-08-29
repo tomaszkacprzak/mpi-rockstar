12ef25ab7eef6454245c91fed771ec9daea67c27
Tomasz Kacprzak <tmkk@pm.me>
2025-08-29 22:15:42 +0200

    Allow example config file via command line

--- examples/example.c
+++ examples/example.c
@@ -1,4 +1,5 @@
 #include <mpi.h>
+#include <stdio.h>
 
 extern "C" {
 #include "config.h"
@@ -7,7 +8,14 @@ extern "C" {
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
-    char cfg[] = "rockstar_lightcone.config";
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <config_file>\n", argv[0]);
+        MPI_Finalize();
+        return 1;
+    }
+
+    char *cfg = argv[1];
     do_config(cfg);
     mpi_main(0, NULL);
     MPI_Finalize();
