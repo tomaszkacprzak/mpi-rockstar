a1cd777a6ce653e5686e4d69745bba8d38965fcd
Tomasz Kacprzak <tmkk@pm.me>
2025-08-31 17:52:49 +0200

    Use single exported entry point

--- demo_librockstar/example.c
+++ demo_librockstar/example.c
@@ -1,14 +1,10 @@
-#include <mpi.h>
-
-/* Declare only the function needed from the library to avoid exposing
- * internal headers. */
+/* Declare only the single entry point from the library to avoid exposing
+ * internal headers or functions. */
 extern "C" {
-int rockstar_mpi_main(int argc, char **argv);
+void rockstar_mpi_main(int argc, char **argv);
 }
 
 int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
     rockstar_mpi_main(argc, argv);
-    MPI_Finalize();
     return 0;
 }
