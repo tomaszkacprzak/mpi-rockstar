0a017663a066d46ef4d897fba6a76fc900bda0ee
Tomasz Kacprzak <tmkk@pm.me>
2025-08-31 17:59:07 +0200

    Initialize MPI in demo

--- demo_librockstar/example.c
+++ demo_librockstar/example.c
@@ -1,10 +1,15 @@
 /* Declare only the single entry point from the library to avoid exposing
- * internal headers or functions. */
+ * internal headers or functions, but initialize MPI locally. */
+
+#include <mpi.h>
+
 extern "C" {
 void rockstar_mpi_main(int argc, char **argv);
 }
 
 int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
     rockstar_mpi_main(argc, argv);
+    MPI_Finalize();
     return 0;
 }
