a68629cabd614167c49c1c8c3964996636fc285d
Tomasz Kacprzak <tmkk@pm.me>
2025-09-02 17:57:32 +0200

    fix pyproject config for editable build

--- pympirockstar/build.py
+++ pympirockstar/build.py
@@ -1,19 +1,24 @@
 import os
-from setuptools import Extension
+from pathlib import Path
+from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
 
 
-def get_extensions():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    lib_dir = os.path.join(root_dir, "src")
-    lib_name = os.environ.get("ROCKSTAR_LIB", "mpi_rockstar")
+class build_ext(_build_ext):
+    """Custom build_ext to cythonize and link against Rockstar."""
 
-    ext = Extension(
-        name="pympirockstar._rockstar",
-        sources=["_rockstar.pyx"],
-        language="c++",
-        include_dirs=[lib_dir],
-        library_dirs=[lib_dir],
-        libraries=[lib_name],
-    )
-    return cythonize([ext])
+    def finalize_options(self):
+        super().finalize_options()
+        # Transform any Cython sources into C/C++ code
+        self.extensions = cythonize(self.extensions)
+
+    def build_extensions(self):
+        root_dir = Path(__file__).resolve().parents[1]
+        lib_dir = root_dir / "src"
+        lib_name = os.environ.get("ROCKSTAR_LIB", "mpi_rockstar")
+        for ext in self.extensions:
+            ext.include_dirs.append(str(lib_dir))
+            ext.library_dirs.append(str(lib_dir))
+            ext.libraries.append(lib_name)
+            ext.language = "c++"
+        super().build_extensions()
