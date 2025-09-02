2761a82e264c1001976064488c3a7fb1b1e27aa2
Tomasz Kacprzak <tmkk@pm.me>
2025-09-02 18:28:01 +0200

    Compile Cython wrapper as C++

--- pympirockstar/build.py
+++ pympirockstar/build.py
@@ -9,13 +9,23 @@ class build_ext(_build_ext):
 
     def finalize_options(self):
         super().finalize_options()
-        # Transform any Cython sources into C/C++ code
-        self.extensions = cythonize(self.extensions)
+        # Transform any Cython sources into C++ code so we can link against
+        # the Rockstar library.  cythonize will generate ``.cpp`` sources when
+        # ``language="c++"`` is specified which avoids compilation errors from
+        # C++ headers being included in ``.c`` files.
+        self.extensions = cythonize(self.extensions, language="c++")
 
     def build_extensions(self):
         root_dir = Path(__file__).resolve().parents[1]
         lib_dir = root_dir / "src"
         lib_name = os.environ.get("ROCKSTAR_LIB", "mpi_rockstar")
+        # Use the MPI C++ compiler if provided so the extension can link
+        # against MPI-enabled Rockstar builds.
+        mpicxx = os.environ.get("MPICC") or os.environ.get("MPICXX")
+        if mpicxx and hasattr(self, "compiler"):
+            self.compiler.set_executable("compiler_cxx", mpicxx)
+            self.compiler.set_executable("linker_so", mpicxx)
+
         for ext in self.extensions:
             ext.include_dirs.append(str(lib_dir))
             ext.library_dirs.append(str(lib_dir))
